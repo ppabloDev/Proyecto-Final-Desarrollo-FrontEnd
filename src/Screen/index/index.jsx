bbf8780c9d34561511e6a2ef4527e16d897e5f22
ppablo.dev <pablo.fause.sarmiento@gmail.com>
2025-08-03 13:24:14 -0300

    Version beta 0.5.1 - minnor bug fix

--- src/Screen/index/index.jsx
+++ src/Screen/index/index.jsx
@@ -14,7 +14,7 @@ const Index = () => {
             </div>
 
             <div>
-                <span className="promo">Desing by <a href="https://www.instagram.com/ppablo.dev/"> @ppablo.dev</a></span>
+                <span className="promo">Desing by <a style={{ marginLeft: "5px" }} href="https://www.instagram.com/ppablo.dev/"> @ppablo.dev</a></span>
             </div>
         </div>
     );
